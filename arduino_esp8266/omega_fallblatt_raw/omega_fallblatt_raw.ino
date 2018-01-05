09818abd426a77118d48b413deb939bcd8951372
Benjamin Kunz <benjamin.kunz@gmail.com>
2018-01-05 20:55:34 +0100

    webserver stuff

--- arduino_esp8266/omega_fallblatt_raw/omega_fallblatt_raw.ino
+++ arduino_esp8266/omega_fallblatt_raw/omega_fallblatt_raw.ino
@@ -1,3 +1,10 @@
+extern "C" {
+#include "user_interface.h"
+}
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <FS.h>
+#include <EEPROM.h>
 #include <Ticker.h>
 
 #define FLAPCOUNT 62
@@ -13,6 +20,13 @@
 #define PIN_EC_5 D4
 #define STROBE_READ_DELAY 30
 #define ENCODER_REREAD_DELAY 4
+
+#define STA_HOSTNAME "flipflap"
+
+const char* ssid = "***REMOVED***";
+const char* password = "***REMOVED***";
+
+ESP8266WebServer server(80);
 
 volatile int read_error_count = 0;
 volatile uint8_t encoder_val = 1; // last read value
@@ -33,7 +47,174 @@ void setup() {
   pinMode(PIN_EC_5, INPUT);
   digitalWrite(PIN_TRIAC_N, 1);
   digitalWrite(PIN_STROBE, 0);
+
+  delay(100);
+  Serial.setDebugOutput(true);
+  SPIFFS.begin();
+  {
+    Dir dir = SPIFFS.openDir("/");
+    while (dir.next()) {
+      String fileName = dir.fileName();
+      size_t fileSize = dir.fileSize();
+      Serial.printf("FS File: %s, size: %s\n", fileName.c_str(), String(fileSize).c_str());
+    }
+    Serial.printf("\n");
+  }
+
+  Serial.println();
+  Serial.print( F("Heap: ") ); Serial.println(system_get_free_heap_size());
+  Serial.print( F("Boot Vers: ") ); Serial.println(system_get_boot_version());
+  Serial.print( F("CPU: ") ); Serial.println(system_get_cpu_freq());
+  Serial.print( F("SDK: ") ); Serial.println(system_get_sdk_version());
+  Serial.print( F("Chip ID: ") ); Serial.println(system_get_chip_id());
+  Serial.print( F("Flash ID: ") ); Serial.println(spi_flash_get_id());
+  Serial.print( F("Flash Size: ") ); Serial.println(ESP.getFlashChipRealSize());
+  Serial.print( F("Vcc: ") ); Serial.println(ESP.getVcc());
+  Serial.println();
+
+  WiFi.mode(WIFI_STA);
+  Serial.printf("Connecting to %s\n", ssid);
+  if (String(WiFi.SSID()) != String(ssid)) {
+    WiFi.begin(ssid, password);
+  }
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.print("Connected! Open http://");
+  Serial.print(WiFi.localIP());
+  Serial.println(" in your browser");
+
+  server.on("/formData", HTTP_GET, []() {
+    sendFormData();
+  });
+
+  server.on("/flap", HTTP_GET, []() {
+    sendFlap();
+  });
+
+  server.on("/flap", HTTP_POST, []() {
+    String value = server.arg("value");
+    encoder_desired_val = value.toInt() + 1;
+    digitalWrite(PIN_TRIAC_N, 0);
+  });
+/*
+  server.on("/flapDown", HTTP_POST, []() {
+    encoder_desired_val--;
+    if(encoder_desired_val < 1)
+      encoder_desired_val = FLAPCOUNT;
+    digitalWrite(PIN_TRIAC_N, 0);
+  });
+*/
+  server.serveStatic("/index.htm", SPIFFS, "/index.htm");
+  server.serveStatic("/js", SPIFFS, "/js");
+  server.serveStatic("/", SPIFFS, "/index.htm");
+
+  server.begin();
+
+  Serial.println("HTTP server started");
+
   attachInterrupt(digitalPinToInterrupt(PIN_STROBE), isr_strobe, FALLING);
+}
+
+String flapNames[] = {
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "11",
+  "12",
+  "13",
+  "14",
+  "15",
+  "16",
+  "17",
+  "18",
+  "19",
+  "20",
+  "21",
+  "22",
+  "23",
+  "24",
+  "25",
+  "26",
+  "27",
+  "28",
+  "29",
+  "30",
+  "31",
+  "32",
+  "33",
+  "34",
+  "35",
+  "36",
+  "37",
+  "38",
+  "39",
+  "40",
+  "41",
+  "42",
+  "43",
+  "44",
+  "45",
+  "46",
+  "47",
+  "48",
+  "49",
+  "50",
+  "51",
+  "52",
+  "53",
+  "54",
+  "55",
+  "56",
+  "57",
+  "58",
+  "59",
+  "60",
+  "61",
+  "62"
+};
+
+void sendFormData()
+{
+  String json = "{";
+
+  json += "\"currentFlap\":{";
+  json += "\"index\":" + String(encoder_desired_val - 1);
+  json += ",\"name\":\"" + flapNames[encoder_desired_val - 1] + "\"}";
+
+  json += ",\"flaps\":[";
+  for (uint8_t i = 0; i < FLAPCOUNT; i++)
+  {
+    json += "\"" + flapNames[i] + "\"";
+    if (i < FLAPCOUNT - 1)
+      json += ",";
+  }
+  json += "]";
+
+  json += "}";
+
+  server.send(200, "text/json", json);
+  json = String();
+}
+
+void sendFlap()
+{
+  String json = "{";
+  json += "\"index\":" + String(encoder_desired_val - 1);
+  json += ",\"name\":\"" + flapNames[encoder_desired_val - 1] + "\"";
+  json += "}";
+  server.send(200, "text/json", json);
+  json = String();
 }
 
 void readEncoder() {
@@ -41,17 +222,17 @@ void readEncoder() {
   int oneflip = prev_val - 1;
   if (oneflip < 1)
     oneflip = FLAPCOUNT;
-    
+
   pinMode(PIN_STROBE, OUTPUT);
   digitalWrite(PIN_STROBE, 0); // pull down strobe, this lets us read the EC pins
-  
+
   encoder_val = digitalRead(PIN_EC_0);
   encoder_val += digitalRead(PIN_EC_1) << 1;
   encoder_val += digitalRead(PIN_EC_2) << 2;
   encoder_val += digitalRead(PIN_EC_3) << 3;
   encoder_val += digitalRead(PIN_EC_4) << 4;
   encoder_val += digitalRead(PIN_EC_5) << 5;
-  
+
   if (encoder_val == 63 || encoder_val != oneflip) { // re-trigger a read if we read an unexpected result. this eliminates the need for real-time finetuning of the read interval.
     encoder_val = prev_val;
     timerReadEncoder.once_ms(ENCODER_REREAD_DELAY, readEncoder);
@@ -62,7 +243,7 @@ void readEncoder() {
   if (encoder_val == encoder_desired_val) {
     digitalWrite(PIN_TRIAC_N, 1);  // stop the motor when we have reached the desired position.
   }
-   
+
   pinMode(PIN_STROBE, INPUT); // strobe back to input, this allows the interrupt to be triggered
   timer_running = false;
   strobe_int = 0;
@@ -74,7 +255,9 @@ void isr_strobe() {
 
 void loop() {
   static int prev_val = 0; // just for printing changes
-  
+
+  server.handleClient();
+
   if (strobe_int > 0 && !timer_running) {
     timer_running = true;
     timerReadEncoder.once_ms(STROBE_READ_DELAY, readEncoder);
@@ -87,12 +270,13 @@ void loop() {
     Serial.print("re ");Serial.println(read_error_count);
     read_error_count = 0;
   }
-  
-  if (encoder_val != prev_val) { 
+
+  if (encoder_val != prev_val) {
     Serial.println(encoder_val);
+    sendFlap();
     prev_val = encoder_val;
   }
-  
+
   if (Serial.available() > 0) {
     char ch = Serial.read();
     if (ch == 'a')
@@ -109,4 +293,3 @@ void loop() {
       digitalWrite(PIN_TRIAC_N, 0);
   }
 }
-
